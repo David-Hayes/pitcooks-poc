96b1a81afdf5b607c035146efcc75ba1395943c5
David Hayes <davidhayes88@gmail.com>
2019-05-14 22:53:29 +0100

    Api routing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
+// routes
+require('./routes/api')(app);
+
 // create port
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`)); // eslint-disable-line no-console
