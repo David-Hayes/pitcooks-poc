a5decf8b6e2d4446d5fc8edb8352b487453f0690
David Hayes <davidhayes88@gmail.com>
2019-07-02 11:53:30 +0100

    Fixed the build

--- server/routes/api.js
+++ server/routes/api.js
@@ -28,41 +28,48 @@ module.exports = (app, passport) => {
   app.post(`${apiRoute}/user/login`, (req, res, next) => {
     passport.authenticate('local', (err, user) => {
       if (err) res.json({ success: false, error_code: 1, error_message: err });
-      if (!user) r
-      // @route   GET /api/user/logout
-      // @desc    Log user out
-      // @access  Public
-      app.get(`${apiRoute}/user/logout`, (req, res) => {
-        req.logout();
+      if (!user) res.json({ success: false, error_code: 3, error_message: 'Username or password is wrong' });
+      req.logIn(user, error => {
+        if (error) res.json({ success: false, error_code: 1, error_message: err });
         res.json({ success: true });
       });
+    })(req, res, next);
+  });
 
-      // @route   POST /api/user/register
-      // @desc    Register new user
-      // @access  Public
-      app.post(`${apiRoute}/user/register`, (req, res) => {
-        // check if user exists already
-        User.findOne({ 'local.email': req.body.email }, (err, user) => {
-          if (user) {
-            res.json({ success: false, error_code: 4, error_message: 'Email already in use' });
-          } else {
-            // create new user
-            const newUser = new User({
-              local: {
-                email: req.body.email,
-                name: req.body.name
-              }
-            });
-            newUser.local.password = newUser.generateHash(req.body.password);
+  // @route   GET /api/user/logout
+  // @desc    Log user out
+  // @access  Public
+  app.get(`${apiRoute}/user/logout`, (req, res) => {
+    req.logout();
+    res.json({ success: true });
+  });
 
-            newUser.save()
-              .then(() => {
-                res.json({ success: true });
-              })
-              .catch(errD => {
-                res.json({ success: false, error_code: 1, error_message: errD });
-              });
+  // @route   POST /api/user/register
+  // @desc    Register new user
+  // @access  Public
+  app.post(`${apiRoute}/user/register`, (req, res) => {
+    // check if user exists already
+    User.findOne({ 'local.email': req.body.email }, (err, user) => {
+      if (user) {
+        res.json({ success: false, error_code: 4, error_message: 'Email already in use' });
+      } else {
+        // create new user
+        const newUser = new User({
+          local: {
+            email: req.body.email,
+            name: req.body.name
           }
         });
-      });
-    };
+        newUser.local.password = newUser.generateHash(req.body.password);
+
+        newUser.save()
+          .then(() => {
+            res.json({ success: true });
+          })
+          .catch(errD => {
+            res.json({ success: false, error_code: 1, error_message: errD });
+          });
+      }
+    });
+  });
+};
